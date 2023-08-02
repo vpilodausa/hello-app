9a51d6f9e87ef35fb2acced81928d2d09ecde7a0
vpilodausa <138843424+vpilodausa@users.noreply.github.com>
2023-08-02 02:07:04 -0500

    Create streamlit_app.py
    quick test code

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+
+import streamlit as st
+import re
+import pandas as pd
+import numpy as np
+
+
+
+
+if __name__ == '__main__':
+    
+    # just copy paste updated euqation file 
+    if st.checkbox('Click me'):
+        st.write('Ma Chuda')
+
+
+
+
